c7b7f80dd1fd5251b6ead663133ec2661c193dfc
kawaemon <me@kawaemon.dev>
2023-05-17 08:40:00 +0900

    opts

--- src/main.rs
+++ src/main.rs
@@ -12,19 +12,37 @@ use rustfft::{
     FftPlanner,
 };
 use sdl2::{
+    audio::{AudioCallback, AudioFormat, AudioSpecDesired},
     event::Event,
     keyboard::Keycode,
     mixer::{InitFlag, Music, AUDIO_F32LSB},
     pixels::Color,
 };
 
-const WIDTH: u32 = 1800;
+const WIDTH: u32 = 2560;
 const HEIGHT: u32 = 900;
 
+const PLAYBACK_SKIP: usize = (40 * FREQUENCY) as usize;
 const FREQUENCY: i32 = 48000;
 const FORMAT: u16 = AUDIO_F32LSB;
 const CHANNELS: i32 = 2;
 const CHUNK_SIZE: i32 = 1024;
+const VOLUME: f32 = 0.5;
+
+const PIXELS_PER_FREQ: usize = 10;
+const SCROLL_SPEED: usize = 30;
+const SHOWN_FREQ_MAX: usize = 5_000;
+
+const SHOWN_VOLUME_MIN: i32 = -30;
+const SHOWN_VOLUME_MAX: i32 = 150;
+const SHOWN_VOLUME_RANGE: u32 = SHOWN_VOLUME_MIN.abs_diff(SHOWN_VOLUME_MAX);
+
+const TIMECHART: bool = false;
+const SPECTRUM: bool = true;
+
+const SAMPLING_WINDOW_SEC: f64 = 10.0 / 60.0;
+const IS_DB: bool = false;
+const GAUSSIAN_SIGMA: f64 = 0.30;
 
 // https://ryo-iijima.com/fftresult/
 fn fft(signal: &[f64], sampling_freq: usize) -> Vec<(f64, f64)> {
@@ -53,14 +71,20 @@ fn fft(signal: &[f64], sampling_freq: usize) -> Vec<(f64, f64)> {
         results.map(move |x| (x as f64) * val)
     };
 
-    let fft_psd_db = fft_data
-        .into_iter()
-        .map(|data| data.abs())
-        //.map(|as_| as_.powf(2.0))
-        //.map(|ps| ps / delta_f)
-        // .map(|psd| 10.0 * psd.log10())
-        .map(|x| x / 10.0)
-        .collect::<Vec<_>>();
+    let fft_psd_db = if IS_DB {
+        fft_data
+            .into_iter()
+            .map(|data| data.abs())
+            .map(|as_| as_.powf(2.0))
+            .map(|ps| ps / delta_f)
+            .map(|psd| 10.0 * psd.log10())
+            .collect::<Vec<_>>()
+    } else {
+        fft_data
+            .into_iter()
+            .map(|data| data.abs())
+            .collect::<Vec<_>>()
+    };
 
     fft_freq.zip(fft_psd_db).collect()
 }
@@ -91,19 +115,17 @@ fn test_fft() {
 fn gauss(x: f64) -> f64 {
     let a = 1.0;
     let mu = 0.0;
-    let sigma = 0.15;
+    let sigma = GAUSSIAN_SIGMA;
     a * (-((x * 2.0 - 1.0) - mu).powf(2.0) / (2.0 * sigma.powf(2.0))).exp()
 }
 
 fn main() {
     let sdl = sdl2::init().unwrap();
     let video = sdl.video().unwrap();
-    let _audio = sdl.audio().unwrap();
+    let audio = sdl.audio().unwrap();
 
     sdl2::mixer::open_audio(FREQUENCY, FORMAT, CHANNELS, CHUNK_SIZE).unwrap();
     let _mixer_context = sdl2::mixer::init(InitFlag::MP3).unwrap();
-
-    let sdl_source = Music::from_file("./out.mp3").unwrap();
 
     let wav = hound::WavReader::open("./out.wav").unwrap();
     let wavspec = wav.spec();
@@ -111,6 +133,7 @@ fn main() {
 
     let (left_samples, _right_samples, _) = wav
         .into_samples::<f32>()
+        .skip(PLAYBACK_SKIP)
         .try_fold(
             (vec![], vec![], true),
             |(mut left, mut right, is_left), v| {
@@ -134,14 +157,44 @@ fn main() {
 
     let mut canvas = window.into_canvas().build().unwrap();
 
-    let mut event_pump = sdl.event_pump().unwrap();
+    let desired_spec = AudioSpecDesired {
+        freq: Some(FREQUENCY),
+        channels: Some(1),
+        samples: None,
+    };
 
-    sdl_source.play(0).unwrap();
+    let played = left_samples.clone();
+    let device = audio
+        .open_playback(None, &desired_spec, |spec| {
+            assert_eq!(spec.format, AudioFormat::F32LSB);
+            assert_eq!(spec.channels, 1);
+            struct Player {
+                pos: usize,
+                data: Vec<f64>,
+            }
+            impl AudioCallback for Player {
+                type Channel = f32;
+                fn callback(&mut self, x: &mut [f32]) {
+                    assert_eq!(x.len(), 4096);
+                    let wave = &self.data[self.pos..self.pos + x.len()];
+                    for (i, x) in x.iter_mut().enumerate() {
+                        *x = wave[i] as f32 * VOLUME;
+                    }
+                    self.pos += x.len();
+                }
+            }
+            Player {
+                pos: 0,
+                data: played,
+            }
+        })
+        .unwrap();
+
+    let mut event_pump = sdl.event_pump().unwrap();
+    device.resume();
     let began_at = Instant::now();
 
     std::thread::sleep(Duration::from_secs_f64(0.5));
-
-    const SCROLL_SPEED: usize = 10;
 
     let mut history = (0..(HEIGHT as usize / SCROLL_SPEED))
         .map(|_| Vec::<(usize, f64)>::new())
@@ -161,9 +214,6 @@ fn main() {
 
         canvas.set_draw_color(Color::BLACK);
         canvas.clear();
-
-        const SHOWN_FREQ_MAX: usize = 5_000;
-        const SAMPLING_WINDOW_SEC: f64 = 15.0 / 60.0;
 
         let bps = wavspec.sample_rate as f64;
         let rel_now = began_at.elapsed().as_secs_f64();
@@ -188,46 +238,40 @@ fn main() {
         let mut prev_vol = fft[0].1;
         let mut chistory = vec![];
 
-        const SHOWN_VOLUME_MIN: i32 = 0;
-        const SHOWN_VOLUME_MAX: i32 = 100;
-        const SHOWN_VOLUME_RANGE: u32 = SHOWN_VOLUME_MIN.abs_diff(SHOWN_VOLUME_MAX);
-        const PIXELS_PER_FREQ: usize = 5;
-
-        for (y, h) in history.iter().enumerate().rev() {
-            if y == 0 {
-                continue;
-            }
-            for &(x, v) in h.iter() {
-                if v < 10.0 {
+        if TIMECHART {
+            for (y, h) in history.iter().enumerate().rev() {
+                if y == 0 {
                     continue;
                 }
-                let rel_volume =
-                    ((f64::clamp(v, SHOWN_VOLUME_MIN as f64, SHOWN_VOLUME_MAX as f64))
-                        + SHOWN_VOLUME_MIN.abs() as f64)
-                        / SHOWN_VOLUME_RANGE as f64;
-                let c: Rgb<u8, LinearRgb> = Hsv::<u8, LinearRgb>::new(
-                    Deg(232),
-                    ((1.0 - rel_volume * 1.5) * 255.0).min(254.0) as u8,
-                    255,
-                )
-                .to_rgb();
-                canvas.set_draw_color(Color::RGB(c.r, c.g, c.b));
-                canvas
-                    .draw_line(
-                        (x as i32, ((y - 1) * SCROLL_SPEED) as i32),
-                        (x as i32, (y * SCROLL_SPEED) as i32),
+                for &(x, v) in h.iter() {
+                    if v < 3.0 {
+                        continue;
+                    }
+                    let rel_volume =
+                        ((f64::clamp(v, SHOWN_VOLUME_MIN as f64, SHOWN_VOLUME_MAX as f64))
+                            + SHOWN_VOLUME_MIN.abs() as f64)
+                            / SHOWN_VOLUME_RANGE as f64;
+                    let c: Rgb<u8, LinearRgb> = Hsv::<u8, LinearRgb>::new(
+                        Deg(232),
+                        ((1.0 - rel_volume * 1.5) * 255.0).min(254.0) as u8,
+                        255,
                     )
-                    .unwrap();
-            }
-            if y * SCROLL_SPEED > HEIGHT as usize {
-                break;
+                    .to_rgb();
+                    canvas.set_draw_color(Color::RGB(c.r, c.g, c.b));
+                    canvas
+                        .draw_line(
+                            (x as i32, ((y - 1) * SCROLL_SPEED) as i32),
+                            (x as i32, (y * SCROLL_SPEED) as i32),
+                        )
+                        .unwrap();
+                }
+                if y * SCROLL_SPEED > HEIGHT as usize {
+                    break;
+                }
             }
         }
 
         for (i, &(freq, mut volume)) in fft.iter().enumerate().skip(1) {
-            volume *= 5.0;
-            volume -= 20.0;
-
             while let Some(fg) = freq_guideline && freq > fg.freq() {
                 freq_guideline = enum_iterator::next(&fg);
                 chistory.push((i * PIXELS_PER_FREQ, volume));
@@ -240,20 +284,22 @@ fn main() {
                 (pos * HEIGHT as f64).clamp(1.0, HEIGHT as f64 - 1.0)
             };
 
-            canvas.set_draw_color(Color::WHITE);
-            canvas
-                .draw_line(
-                    (
-                        ((i - 1) * PIXELS_PER_FREQ) as i32,
-                        HEIGHT as i32 - y(prev_vol) as i32,
-                    ),
-                    (
-                        (i * PIXELS_PER_FREQ) as i32,
-                        HEIGHT as i32 - y(volume) as i32,
-                    ),
-                )
-                .unwrap();
-            prev_vol = volume;
+            if SPECTRUM {
+                canvas.set_draw_color(Color::WHITE);
+                canvas
+                    .draw_line(
+                        (
+                            ((i - 1) * PIXELS_PER_FREQ) as i32,
+                            HEIGHT as i32 - y(prev_vol) as i32,
+                        ),
+                        (
+                            (i * PIXELS_PER_FREQ) as i32,
+                            HEIGHT as i32 - y(volume) as i32,
+                        ),
+                    )
+                    .unwrap();
+                prev_vol = volume;
+            }
 
             if i * PIXELS_PER_FREQ > WIDTH as usize {
                 break;
